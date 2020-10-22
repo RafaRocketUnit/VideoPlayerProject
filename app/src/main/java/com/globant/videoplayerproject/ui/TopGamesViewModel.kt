aac95bb0abac48397cbc585ac5787d99a07d1077
micaela.contreras <micaela.contreras@globant.com>
2020-10-22 14:02:36 -0300

    DTCITA-12 - get Token to retrieve endpoints

--- app/src/main/java/com/globant/videoplayerproject/ui/TopGamesViewModel.kt
+++ app/src/main/java/com/globant/videoplayerproject/ui/TopGamesViewModel.kt
@@ -5,13 +5,22 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.globant.videoplayerproject.api.RepositoryApi
+import com.globant.videoplayerproject.model.AccessToken
 import com.globant.videoplayerproject.model.Data
 import kotlinx.coroutines.launch
 
 class TopGamesViewModel : ViewModel() {
 
+    private val _accessToken = MutableLiveData<AccessToken>()
+    private val _onErrorAccessToken = MutableLiveData<Boolean>()
     private val _listGames = MutableLiveData<List<Data>>()
     private val _onError = MutableLiveData<Boolean>()
+
+    val accessToken: LiveData<AccessToken>
+        get() = _accessToken
+
+    val onErrorAccessToken: LiveData<Boolean>
+        get() = _onErrorAccessToken
 
     val listGames: LiveData<List<Data>>
         get() = _listGames
@@ -20,12 +29,28 @@ class TopGamesViewModel : ViewModel() {
         get() = _onError
 
     init {
-        getListGames()
+        getAccessToken()
     }
 
-    private fun getListGames() {
+    private fun getAccessToken() {
+        val clientId = "xzpd1f4527fu8fct7p7own0pgi35v5"
+        val clientSecret = "171nriwnud80u56cy3m0glkcuj4qew"
+        val grantType = "client_credentials"
         viewModelScope.launch {
-            val getPropertiesDeferred = RepositoryApi.retrofitService.getTopGamesAsync()
+            val getPropertiesDeferred = RepositoryApi.retrofitServiceToken.getAccessToken(clientId, clientSecret, grantType)
+            try {
+                val accessToken = getPropertiesDeferred.await()
+                _accessToken.value = accessToken
+            } catch (e: Exception) {
+                _onErrorAccessToken.value = true
+            }
+
+        }
+    }
+
+    fun getListGames(accessToken: String) {
+        viewModelScope.launch {
+            val getPropertiesDeferred = RepositoryApi.retrofitService.getTopGamesAsync(accessToken)
             try {
                 val topGame = getPropertiesDeferred.await()
                 _listGames.value = topGame.data
